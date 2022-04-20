b0c89d32d718a9bed6626818cca3e36aaa665c1a
anca_maria.ciuciu <anca_maria.ciuciu@fep8.grid.pub.ro>
2022-04-20 19:37:32 +0300

    Some changes

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "BLAS SOLVER"
-./tema2_blas inputs/input_blas
-echo "NEOPT SOLVER"
-./tema2_neopt inputs/input_neopt
-echo "OPT SOLVER"
-./tema2_opt_m inputs/input_opt
+#echo "BLAS SOLVER"
+./../tema2_blas inputs/input_blas
+#echo "NEOPT SOLVER"
+./../tema2_neopt inputs/input_neopt
+#echo "OPT SOLVER"
+./../tema2_opt_m inputs/input_opt
