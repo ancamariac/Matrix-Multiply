1bc7c9e3ac2c405336fd445ed3586ac416cc2861
ancamariac <ciuciuancamaria@gmail.com>
2022-04-22 17:00:26 +0300

    changes

--- solver_neopt.c
+++ solver_neopt.c
@@ -3,6 +3,7 @@
  * 2022 Spring
  */
 #include "utils.h"
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 /*
  * Add your unoptimized implementation here
@@ -87,6 +88,7 @@ double *multiplication_upper(int N, double *A, double *U) {
 	return R;
 }
 
+// L = A transposed
 double *multiplication_lower(int N, double *A, double *L) {
 
 	double *R = (double*)calloc(N * N, sizeof(double));
@@ -97,8 +99,8 @@ double *multiplication_lower(int N, double *A, double *L) {
 
 	for (register int i = 0; i < N; i++) {
 		for (register int j = 0; j < N; j++) {
-			for (register int k = j; k < N; k++) {
-				R[i * N + j] += A[i * N + k] * L[k * N + j];
+			for (register int k = 0; k < MIN(i, j); k++) {
+				R[i * N + j] += A[k * N + i] * L[k * N + j];
 			}
 		}
 	}
@@ -141,12 +143,12 @@ double* my_solver(int N, double *A, double* B) {
 	if (R1 == NULL)
 		return NULL;
 
-	// At = A transpus
+	/*// At = A transpus
 	double *At = transpose_upper(N, A);
 
 	if (At == NULL)
 		return NULL;
-
+*/
 	// Bt = B transpus
 	double *Bt = transposed(N, B);
 
@@ -154,7 +156,7 @@ double* my_solver(int N, double *A, double* B) {
 		return NULL;
 
 	// R2 = R1 * At -> R2 = B x A x At
-	double *R2 = multiplication_lower(N, R1, At);
+	double *R2 = multiplication_lower(N, R1, A);
 
 	if (R2 == NULL)
 		return NULL;
@@ -174,8 +176,8 @@ double* my_solver(int N, double *A, double* B) {
 	free(R1);
 	free(R2);
 	free(R3);
-	free(At);
-	free(Bt);
+	//free(At);
+	//free(Bt);
 	
 	return R4;
 }
